23aae69cb4d5b4eb8ba4dc06d45236c32bb4d719
soso <>
2018-06-12 15:19:34 +0800

    方便调试

--- Multithreading/OperationViewController.m
+++ Multithreading/OperationViewController.m
@@ -22,33 +22,13 @@
     
     
     //队列挂起初始化
-    [self createOperationQueueSuspended];
-
-   
-}
-
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    
-    
-//    [self invocationOperation];
-//    [self blockOperation];
-//
-//    [self creatOperationQueue1];
-//    [self creatOperationQueue2];
-    
-    //设置挂起
-//    [self operationSetSuspended];
-    
-    //设置依赖
-//    [self createOperationQueueSetDependency];
-
-    
-    [self createOperationQueueShowImage];
+    //    [self createOperationQueueSuspended];
 }
 
 
 #pragma mark-invocationOperation
--(void)invocationOperation{
+- (IBAction)invocationOperation:(id)sender {
+    
     //初始化Operation子类
     NSInvocationOperation *operation = [[NSInvocationOperation alloc]initWithTarget:self selector:@selector(run) object:nil];
     //开启
@@ -59,7 +39,7 @@
 }
 
 #pragma mark-blockOperation
--(void)blockOperation{
+- (IBAction)blockOperation:(id)sender {
     
     //初始化Operation子类
     NSBlockOperation *operation = [NSBlockOperation blockOperationWithBlock:^{
@@ -71,10 +51,13 @@
     [operation addExecutionBlock:^{
         NSLog(@"--%@",[NSThread currentThread]);
     }];
+    
+    [operation start];
 }
 
 #pragma mark-OperationQueue基本用法
--(void)creatOperationQueue1{
+- (IBAction)creatOperationQueue1:(id)sender {
+    
     //创建队列
     NSOperationQueue *queue = [[NSOperationQueue alloc]init];
     
@@ -102,10 +85,7 @@
     //也可以直接创建任务到队列中去
     [queue addOperationWithBlock:^{
         NSLog(@"-6--%@",[NSThread currentThread]);
-
     }];
-
-
 }
 
 -(void)run1{
@@ -117,14 +97,18 @@
 }
 
 
+
+
 #pragma mark-OperationQueue相关设置--设置最大并发操作数
--(void)creatOperationQueue2{
+- (IBAction)creatOperationQueue2:(id)sender {
     //创建队列
     NSOperationQueue *queue = [[NSOperationQueue alloc]init];
     //设置最大并发操作数(不管加入队列有多少操作，实际队列并发数为2)
-    queue.maxConcurrentOperationCount = 2;
-//    queue.maxConcurrentOperationCount = 1;  //设置为1就成了串行队列
-
+//    queue.maxConcurrentOperationCount = 2;
+    
+    //设置为1就成了串行队列
+    queue.maxConcurrentOperationCount = 1;
+    
     [queue addOperationWithBlock:^{
         NSLog(@"-1--%@",[NSThread currentThread]);
     }];
@@ -140,38 +124,38 @@
 }
 
 
+
 #pragma mark-OperationQueue相关设置--设置队列挂起（暂停）
--(void)createOperationQueueSuspended{
+- (IBAction)createOperationQueueSuspended:(id)sender {
+
     //创建队列
-    NSOperationQueue *queue = [[NSOperationQueue alloc]init];
-    queue.maxConcurrentOperationCount = 1;
-    [queue addOperationWithBlock:^{
+    self.queue = [[NSOperationQueue alloc]init];
+    self.queue.maxConcurrentOperationCount = 1;
+    [self.queue addOperationWithBlock:^{
         NSLog(@"-1--%@",[NSThread currentThread]);
         [NSThread sleepForTimeInterval:1.0];
     }];
-    [queue addOperationWithBlock:^{
+    [self.queue addOperationWithBlock:^{
         NSLog(@"-2--%@",[NSThread currentThread]);
         [NSThread sleepForTimeInterval:1.0];
     }];
-    [queue addOperationWithBlock:^{
+    [self.queue addOperationWithBlock:^{
         NSLog(@"-3--%@",[NSThread currentThread]);
         [NSThread sleepForTimeInterval:1.0];
     }];
-    [queue addOperationWithBlock:^{
+    [self.queue addOperationWithBlock:^{
         NSLog(@"-4--%@",[NSThread currentThread]);
         [NSThread sleepForTimeInterval:1.0];
     }];
     //设置队列挂起或者取消的话都必须是在block方法执行完之后才有效
-    [queue addOperationWithBlock:^{
-        for(NSInteger i = 0;i<20;i++){
-            NSLog(@"-5--%ld---%@",(long)i,[NSThread currentThread]);
+    [self.queue addOperationWithBlock:^{
+        for(NSInteger i = 0;i<5;i++){
+            NSLog(@"-5--%zd---%@",(long)i,[NSThread currentThread]);
         }
     }];
-    self.queue = queue;
 }
-
 #pragma mark-设置队列挂起
--(void)operationSetSuspended{
+- (IBAction)operationSetSuspended:(id)sender {
     if(self.queue.suspended){
         //恢复队列，继续执行
         self.queue.suspended  = NO;
@@ -180,20 +164,19 @@
         self.queue.suspended  = YES;
     }
 }
-
 #pragma mark-设置队列取消（取消就意味着后续队列不再执行，再次启动需要重新加入队列）
--(void)operationSetCancel{
-
+- (IBAction)operationSetCancel:(id)sender {
     [self.queue cancelAllOperations];
 }
 
 
 
+
 #pragma mark-OperationQueue相关设置--设置队列依赖
--(void)createOperationQueueSetDependency{
-    
+- (IBAction)createOperationQueueSetDependency:(id)sender {
+
     NSOperationQueue *queue = [[NSOperationQueue alloc]init];
-   
+    
     NSBlockOperation *op1 = [NSBlockOperation blockOperationWithBlock:^{
         NSLog(@"down1---%@",[NSThread currentThread]);
     }];
@@ -212,20 +195,18 @@
     [queue addOperation:op2];
     [queue addOperation:op3];
     
-
+    
     //监听一个操作的执行完成
     [op3 setCompletionBlock:^{
         NSLog(@"执行完成");
     }];
 }
 
-
-
 #pragma mark-线程间的通信
--(void)createOperationQueueShowImage{
+- (IBAction)createOperationQueueShowImage:(id)sender {
     
     NSOperationQueue *queue = [[NSOperationQueue alloc]init];
-   
+    
     __block UIImage *image1;
     NSBlockOperation *downloadw1 = [NSBlockOperation blockOperationWithBlock:^{
         
@@ -242,7 +223,7 @@
         NSURL *url = [NSURL URLWithString:@"http://pic38.nipic.com/20140228/5571398_215900721128_2.jpg"];
         NSData *data = [NSData dataWithContentsOfURL:url];
         image2 =[UIImage imageWithData:data];
-
+        
     }];
     
     
@@ -266,7 +247,7 @@
     [queue addOperation:downloadw1];
     [queue addOperation:downloadw2];
     [queue addOperation:combine];
-
+    
 }
 
 - (void)didReceiveMemoryWarning {
